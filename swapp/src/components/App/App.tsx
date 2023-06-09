ec04446a7086a39b1cce71f7c9118271c5d11937
alicefirminger <alicefirminger@gmail.com>
2023-06-09 14:36:27 +0100

    commented out typescript in app.tsx

--- swapp/src/components/App/App.tsx
+++ swapp/src/components/App/App.tsx
@@ -2,9 +2,9 @@
 
 import React from 'react'
 
-type Props = {}
+// type Props = {}
 
-export default function index({}: Props) {
+export default function App() {
   return (
     <div className="App">
       <h1>SWAPP bit by bit!</h1>
