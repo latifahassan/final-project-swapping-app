371835e76713a802e034525e13b8fb3bd8d022bc
alicefirminger <alicefirminger@gmail.com>
2023-06-09 14:30:20 +0100

    added ! and ran npm run build

--- swapp/src/components/App/App.tsx
+++ swapp/src/components/App/App.tsx
@@ -7,7 +7,7 @@ type Props = {}
 export default function index({}: Props) {
   return (
     <div className="App">
-      <h1>SWAPP bit by bit</h1>
+      <h1>SWAPP bit by bit!</h1>
     </div>
   )
 }
