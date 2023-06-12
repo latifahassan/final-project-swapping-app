6c40ec20a6cf182120a6c4d40deadbdfc9cd4fa1
alicefirminger <alicefirminger@gmail.com>
2023-06-12 11:43:50 +0100

    made dummy data json and started writing search function in SearchBar

--- swapp/src/components/SearchBar/SearchBar.tsx
+++ swapp/src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,30 @@
 // this is a dummy file
-
+import listingsData from './data.json'
 import React from 'react'
+import { useState } from 'react'
 
-type Props = {}
+type Listing = {
+  title:string,
+  username: string,
+  image: string
+}
 
-export default function SearchBar({}: Props) {
+export default function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<Listing[]>([]);
+  function handleSearch(){
+    const filteredData = listingsData.filter((listing:Listing)=> listing.title.toLowerCase().includes(searchQuery.toLowerCase()));
+    setSearchResults(filteredData)
+  }
   return (
     <div>SearchBar</div>
   )
 }
+
+/*
+- import data ✅
+- declare states ✅
+  - search query - stores what user types in
+  - search results - shows result 
+- write searchbar function
+ */
