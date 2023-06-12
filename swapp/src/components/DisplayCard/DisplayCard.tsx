e562f09b3917a7806b19c1f06719c01bbc590fdc
gregor <gregor.aubrey.uk@gmail.com>
2023-06-12 11:14:33 +0100

    added props to DisplayCard and ListDisplay. Made a map function on ListDisplay to print a certain number of DisplayCard components

--- swapp/src/components/DisplayCard/DisplayCard.tsx
+++ swapp/src/components/DisplayCard/DisplayCard.tsx
@@ -1,11 +1,19 @@
-// this is a dummy file
-
 import React from 'react'
 
-type Props = {}
+type DisplayCardProps = {
+  image: string
+  title: string
+  username: string
+  key: number
+}
 
-export default function DisplayCard({}: Props) {
+export default function DisplayCard({image, title, username, key}: DisplayCardProps) {
   return (
-    <div>DisplayCard</div>
+    <>
+      <img src={image} alt={`image of ${title}`} />
+      <h3>{title}</h3>
+      <p>{username}</p>
+      <button type="button">GET IT NOW</button>
+    </>
   )
 }
--- swapp/src/components/ListDisplay/ListDisplay.tsx
+++ swapp/src/components/ListDisplay/ListDisplay.tsx
@@ -1,11 +1,30 @@
-// this is a dummy file
-
 import React from 'react'
+import DisplayCard from '../DisplayCard/DisplayCard'
 
-type Props = {}
 
-export default function ListDisplay({}: Props) {
+// ListDisplay should receive a prop (from App) of the number of DisplayCard items to display
+type ListDisplayProps = {
+  numItems: number
+  itemsArray: {title: string, image: string, username: string}[]
+}
+
+export default function ListDisplay({numItems, itemsArray}: ListDisplayProps) {
+  // the return statement should return a list of DisplayCard items based on the numItems and itemsArray props
+  const slicedItemsArray = itemsArray.slice(0, numItems)
+
   return (
-    <div>ListDisplay</div>
+    <>
+      {slicedItemsArray.map((item, index) => {
+        return (
+          <DisplayCard
+            key={index}
+            image={item.image}
+            title={item.title}
+            username={item.username}
+          />
+        )
+      }
+      )}
+    </>
   )
 }
