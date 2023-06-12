af4bdafb6fb293834ba31a6752fb10aaa2cabf64
alicefirminger <alicefirminger@gmail.com>
2023-06-12 15:31:08 +0100

    wrote 3 tests for searchBar component

--- swapp/src/components/SearchBar/SearchBar.test.tsx
+++ swapp/src/components/SearchBar/SearchBar.test.tsx
@@ -1,5 +1,5 @@
 import SearchBar from "./SearchBar";
-import { render, fireEvent } from "@testing-library/react"
+import { render, fireEvent, screen } from "@testing-library/react"
 
 
 //🧪 check that the search query is updated with the input and displays the search results
@@ -7,14 +7,24 @@ import { render, fireEvent } from "@testing-library/react"
 
 
 describe ('SearchBar component', ()=> {
-it("should update search query and display search results", ()=> {
-    const { getByPlaceholderText, getByText } = render(<SearchBar />) 
-    const inputElement = getByPlaceholderText('Search')
+test("update search query state on input change", ()=> {
+    render(<SearchBar />) 
+    const inputElement = screen.getByPlaceholderText('Search')
     fireEvent.change(inputElement, { target: { value: 'jumper' } })
+    expect(inputElement.value).toBe('jumper');
 }) 
+test("displays search result header on page", ()=> {
+    render(<SearchBar />)
+    const inputElement = screen.getByPlaceholderText('Search')
+    fireEvent.change(inputElement, { target: { value: 'jumper' } })
+    const resultHeader = screen.getByText('Search Results:')
+    expect(resultHeader).toBeInTheDocument();
+})
+test("displays a search result item on page", ()=> {
+    render(<SearchBar />)
+    const inputElement = screen.getByPlaceholderText('Search')
+    fireEvent.change(inputElement, { target: { value: 'jumper' } })
+    const searchResultItem = screen.getByAltText('Knitted jumper in maroon')
+    expect(searchResultItem).toBeInTheDocument();
 }
-)
-
-
-
-
+)})
