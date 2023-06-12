acb4247c4fb45243d2bc5cb692f7c5074187a8fc
gregor <gregor.aubrey.uk@gmail.com>
2023-06-12 12:03:50 +0100

    finished writing tests for ListDisplay

--- swapp/src/components/ListDisplay/ListDisplay.test.tsx
+++ swapp/src/components/ListDisplay/ListDisplay.test.tsx
@@ -11,13 +11,21 @@ jest.mock('../DisplayCard/DisplayCard', () => {
 describe("ListDisplay", () => {
     test('renders correct number of DisplayCards', () => {
         render(<ListDisplay numItems={3} itemsArray={[
-          {title: 'test title 1', image: 'test1.jpg', username: 'test username 1'},
-          {title: 'test title 2', image: 'test2.jpg', username: 'test username 2'},
-          {title: 'test title 3', image: 'test3.jpg', username: 'test username 3'}
+          {title: 'test title 1', image: 'test1.jpg', username: 'test username 1', id: 'abc123'},
+          {title: 'test title 2', image: 'test2.jpg', username: 'test username 2', id: 'def456'},
+          {title: 'test title 3', image: 'test3.jpg', username: 'test username 3', id: 'ghi789'}
         ]} />);
         expect(DisplayCard).toHaveBeenCalledTimes(3);
       });
+
+    test('Passes correct props to DisplayCard', () => {
+        render(<ListDisplay numItems={1} itemsArray={[
+            title: 'test title 1', image: 'test1.jpg', username: 'test username 1', id: 'abc123'
+        ]} />);
+        expect(DisplayCard).toHaveBeenCalledWith({title: 'test title 1', image: 'test1.jpg', username: 'test username 1', id: 'abc123'});
+    })
+    
+})
     
     
-    
-    
+
