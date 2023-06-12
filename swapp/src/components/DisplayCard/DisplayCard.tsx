1351b899c449e5156f9b95ab4a644ab1e5b87957
JownJawge <jgwoodard1718@gmail.com>
2023-06-12 11:22:41 +0100

    edited alt text for image

--- swapp/src/components/DisplayCard/DisplayCard.tsx
+++ swapp/src/components/DisplayCard/DisplayCard.tsx
@@ -10,7 +10,7 @@ type DisplayCardProps = {
 export default function DisplayCard({image, title, username, key}: DisplayCardProps) {
   return (
     <>
-      <img src={image} alt={`image of ${title}`} />
+      <img src={image} alt={`img of ${title}`} />
       <h3>{title}</h3>
       <p>{username}</p>
       <button type="button">GET IT NOW</button>
