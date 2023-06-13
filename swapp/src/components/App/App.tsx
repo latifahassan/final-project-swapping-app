86b8c1ea61b66762cf5a50f40c0e4517d3928a85
latifahassan <latifahassanali@outlook.com>
2023-06-13 10:32:55 +0100

    sorted value out

--- swapp/src/components/App/App.tsx
+++ swapp/src/components/App/App.tsx
@@ -1,15 +1,11 @@
 // THIS IS THE APP
 
-import React from 'react'
-import SearchBar from '../SearchBar/SearchBar'
 
 // type Props = {}
-
 export default function App() {
   return (
     <div className="App">
       <h1>SWAPP bit by bit!!!!</h1>
-      <SearchBar/>
     </div>
-  )
+  );
 }
