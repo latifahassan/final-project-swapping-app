1de87f58a5da1bb60aae5ad744558cc08ae0b02a
NeilBrooks <neil.brooks87@gmail.com>
2023-06-13 12:18:38 +0100

    Added barebones testing skeleton for PopUp

--- swapp/src/components/PopUp/PopUp.test.tsx
+++ swapp/src/components/PopUp/PopUp.test.tsx
@@ -1,3 +1,17 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import PopUp from '../PopUp/PopUp'
+
+jest.mock('../PopUp/PopUp', () => {
+    return jest.fn(() => null);
+    }
+);
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
+// describe("PopUp", () => {
+//     test('Contains title', () => {
+//         const actual = render(<PopUp/>);
+//       });
