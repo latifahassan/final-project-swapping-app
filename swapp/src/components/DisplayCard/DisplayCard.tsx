5b76a83f1582783f008ed56ad6a32e9cd53140b2
NeilBrooks <neil.brooks87@gmail.com>
2023-06-13 14:33:13 +0100

    Added tests within PopUp for basic functionality and handleclick. Commented out tests in ListDisplay and DisplayCard as they were interfering wtih tests run

--- swapp/src/components/DisplayCard/DisplayCard.tsx
+++ swapp/src/components/DisplayCard/DisplayCard.tsx
@@ -52,13 +52,13 @@ export default function DisplayCard({id, image, title, username, handleGetItNowC
       <img src={image} alt={`img of ${title}`} />
       <h3>{title}</h3>
       <p>{username}</p>
-      {!spendATokenClicked && <button onClick={handleGetItNowClick} className='greenButton'>GET IT NOW</button>} 
+      {!spendATokenClicked && <button onClick={handleGetItNowClick} id='greenButton'>GET IT NOW</button>} 
              {/*
              The GET IT NOW button should bring up a PopUp asking you to confirm your address
              It will do this by calling a function from App.tsx that will set the state of the PopUp to true
              If loggedIn is false, the button should take you to the sign up/log in page        
              */}
-      {spendATokenClicked && <p>Claimed</p>} 
+      {spendATokenClicked && <p id = "claimedOption">Claimed</p>} 
       <p>The ID is {id}</p>
     </>
   )
