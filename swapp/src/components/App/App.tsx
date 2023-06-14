21abef3cb5a8d2f972d5a601e46f8f227fba6ae4
gregor <gregor.aubrey.uk@gmail.com>
2023-06-14 16:04:03 +0100

    added NavBar code taken from codesandbox and then adapted it for our needs

--- swapp/src/components/App/App.tsx
+++ swapp/src/components/App/App.tsx
@@ -1,14 +1,14 @@
 // THIS IS THE APP
 
 import HomePage from '../HomePage/HomePage'
+import NavBar from '../NavBar/NavBar'
 // type Props = {}
 export default function App() {
   return (
     <div className="App">
-
+      <NavBar />
       <h1>SWAPP bit by bit-test FINAL - 17:10</h1>
       <HomePage />
-
     </div>
   );
 }
