b100ecc5ddfdd28903e01bee0e9624965a0da768
gregor <gregor.aubrey.uk@gmail.com>
2023-06-15 15:02:14 +0100

    commented out routes for which we have yet to write code

--- swapp/src/components/App/App.tsx
+++ swapp/src/components/App/App.tsx
@@ -6,6 +6,7 @@ import NavBar from '../NavBar/NavBar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
 
+
 export default function App() {
   return (
     <Router>
