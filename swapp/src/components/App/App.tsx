ac7594707a1ddf9a2ecf8a5291af3910556fee83
gregor <gregor.aubrey.uk@gmail.com>
2023-06-15 14:58:48 +0100

    commented out the routes for which we have not yet written any code

--- swapp/src/components/App/App.tsx
+++ swapp/src/components/App/App.tsx
@@ -1,24 +1,22 @@
-// THIS IS THE APP
-
 import HomePage from '../HomePage/HomePage';
-import LandingPage from '../LandingPage/LandingPage';
-import AuthPage from '../AuthPage/AuthPage';
-import MyAccountPage from '../MyAccountPage/MyAccountPage';
+// import LandingPage from '../LandingPage/LandingPage';
+// import AuthPage from '../AuthPage/AuthPage';
+// import MyAccountPage from '../MyAccountPage/MyAccountPage';
 import NavBar from '../NavBar/NavBar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 export default function App() {
   return (
     <Router>
-    <div className="App">
-      <NavBar />
-        <Routes>
-          <Route path = "/home" element = {<HomePage/>} />
-          <Route path = "/" element = {<LandingPage/>} />
-          <Route path = "/login" element = {<AuthPage/>} />
-          <Route path = "/myaccount" element = {<MyAccountPage/>} />
-      </Routes>
-    </div>
+      <div className="App">
+        <NavBar />
+          <Routes>
+            <Route path = "/home" element = {<HomePage/>} />
+            {/* <Route path = "/" element = {<LandingPage/>} /> */}
+            {/* <Route path = "/login" element = {<AuthPage/>} /> */}
+            {/* <Route path = "/myaccount" element = {<MyAccountPage/>} /> */}
+        </Routes>
+      </div>
     </Router>
   );
 }
