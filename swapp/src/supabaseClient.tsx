12e8a1b7390f1d9ae4f50d8a6d1b62b210972467
latifahassan <latifahassanali@outlook.com>
2023-06-20 10:40:34 +0100

    added supabase

---
+++ swapp/src/supabaseClient.tsx
@@ -0,0 +1,4 @@
+import {createClient} from '@supabase/supabase-js';
+const supabaseUrl:string =  process.env.REACT_APP_SUPABASE_URL as string
+const supabaseAnonKey:string = process.env.REACT_APP_SUPABASE_ANON_KEY as string
+export default createClient(supabaseUrl,supabaseAnonKey)
