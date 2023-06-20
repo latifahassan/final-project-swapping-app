422905a85e83b7e49accbdf3d8732cbf7105e92a
NeilBrooks <neil.brooks87@gmail.com>
2023-06-20 16:17:37 +0100

    Reactivated useEffect for login - found the error - it was autoredirecting because the session was being saved to our browser. New login possible on incognito

--- swapp/src/components/AuthPage/AuthPage.tsx
+++ swapp/src/components/AuthPage/AuthPage.tsx
@@ -1,46 +1,8 @@
-// import supabase from '../../supabaseClient'
-// import { useState, useEffect } from 'react'
-// import { Auth } from '@supabase/auth-ui-react'
-// import { ThemeSupa } from '@supabase/auth-ui-shared'
-
-// type Session = {
-//   user?: {
-//     email?: string
-//     id?: string
-//   }
-// }
-
-// export default function Authpage() {
-//   const [session, setSession] = useState<Session | null | undefined>(null)
-
-//   useEffect(() => {
-//     supabase.auth.getSession().then(({ data: session }) => {
-//       setSession(session)
-//     })
-  
-//     const { data: subscription } = supabase.auth.onAuthStateChange((_event, session) => {
-//       setSession(session)
-//     })
-  
-//     return () => {
-//       if (subscription) {
-//         subscription.data?.unsubscribe()
-//       }
-//     }
-//   }, [])
-
-//   if (!session) {
-//     return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-//   } else {
-//     return <div>Logged in!</div>
-//   }
-// }
-
 import supabase from '../../supabaseClient'
 import { useState, useEffect } from 'react'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-import HomePage from '../HomePage/HomePage'
+import { useNavigate } from 'react-router-dom'
 
 interface Session {
   user?: {
@@ -49,14 +11,12 @@ interface Session {
 }
 
 export default function AuthPage() {
-
-
-
-
+  const navigate = useNavigate();
   const [session, setSession] = useState<Session | null>(null)
-
   useEffect(() => {
+    console.log('useEffect running')
     supabase.auth.getSession().then(({ data: { session } }) => {
+      console.log(session)
       setSession(session)
     })
 
@@ -69,14 +29,17 @@ export default function AuthPage() {
     return () => subscription.unsubscribe()
   }, [])
 
+  useEffect(() => {
+    if (session) {
+      console.log("Session exists")
+      navigate('/home');
+    }
+  }, [session, navigate])
+  
   if (!session) {
+    console.log("Rendering auth component")
     return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-  }
-  else {
-    return (<div>
-      <HomePage />
-    </div>)
+  } else {
+    return null
   }
 }
-
-// Test
