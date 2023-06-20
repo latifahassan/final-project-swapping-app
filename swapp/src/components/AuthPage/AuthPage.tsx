be8e76c2fd54343231fd19ed37910e946e97aa7b
latifahassan <latifahassanali@outlook.com>
2023-06-20 12:09:22 +0100

    started on authpage

--- swapp/src/components/AuthPage/AuthPage.tsx
+++ swapp/src/components/AuthPage/AuthPage.tsx
@@ -1,9 +1,35 @@
-// this is a dummy file
+import supabase from '../../supabaseClient'
+import { useState, useEffect } from 'react'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-import React from 'react'
+type Session = {
+  user: {
+    email: string
+    id: string
+  }
+}
 
 export default function Authpage() {
-  return (
-    <div>Authpage</div>
-  )
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const { data: subscription } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+  } else {
+    return <div>Logged in!</div>
+  }
 }
+
+
