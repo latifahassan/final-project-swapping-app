b4089ee5707bb4371a8d0eacee5d3a16fc4eed2c
NeilBrooks <neil.brooks87@gmail.com>
2023-06-20 13:53:50 +0100

    Setup login with homepage render

--- swapp/src/components/AuthPage/AuthPage.tsx
+++ swapp/src/components/AuthPage/AuthPage.tsx
@@ -1,37 +1,80 @@
+// import supabase from '../../supabaseClient'
+// import { useState, useEffect } from 'react'
+// import { Auth } from '@supabase/auth-ui-react'
+// import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+// type Session = {
+//   user?: {
+//     email?: string
+//     id?: string
+//   }
+// }
+
+// export default function Authpage() {
+//   const [session, setSession] = useState<Session | null | undefined>(null)
+
+//   useEffect(() => {
+//     supabase.auth.getSession().then(({ data: session }) => {
+//       setSession(session)
+//     })
+  
+//     const { data: subscription } = supabase.auth.onAuthStateChange((_event, session) => {
+//       setSession(session)
+//     })
+  
+//     return () => {
+//       if (subscription) {
+//         subscription.data?.unsubscribe()
+//       }
+//     }
+//   }, [])
+
+//   if (!session) {
+//     return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+//   } else {
+//     return <div>Logged in!</div>
+//   }
+// }
+
 import supabase from '../../supabaseClient'
 import { useState, useEffect } from 'react'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
+import HomePage from '../HomePage/HomePage'
 
-type Session = {
-  user: {
-    email: string
-    id: string
-  }
+interface Session {
+  user?: {
+    email?: string;
+  };
 }
 
-export default function Authpage() {
-  const [session, setSession] = useState<Session | null | undefined>(null)
+export default function AuthPage() {
+
+
+
+
+  const [session, setSession] = useState<Session | null>(null)
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: session }) => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
     })
-  
-    const { data: subscription } = supabase.auth.onAuthStateChange((_event, session) => {
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session)
     })
-  
-    return () => {
-      if (subscription) {
-        subscription.data?.unsubscribe()
-      }
-    }
+
+    return () => subscription.unsubscribe()
   }, [])
 
   if (!session) {
-    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-  } else {
-    return <div>Logged in!</div>
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>
+      <HomePage />
+    </div>)
   }
 }
