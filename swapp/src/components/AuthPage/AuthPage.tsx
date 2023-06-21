5cfda8e328a0dd72545f778837c36cb856b05839
NeilBrooks <neil.brooks87@gmail.com>
2023-06-21 10:52:14 +0100

    Attempted to link navbar into authpage in order to pass logout function

--- swapp/src/components/AuthPage/AuthPage.tsx
+++ swapp/src/components/AuthPage/AuthPage.tsx
@@ -3,13 +3,16 @@ import { useState, useEffect } from 'react'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useNavigate } from 'react-router-dom'
-
+import NavBar from '../NavBar/NavBar'
 interface Session {
   user?: {
     email?: string;
   };
 }
 
+type Props = {
+  handleLogout: () => void;
+}
 export default function AuthPage() {
   const navigate = useNavigate();
   const [session, setSession] = useState<Session | null>(null)
@@ -28,6 +31,13 @@ export default function AuthPage() {
 
     return () => subscription.unsubscribe()
   }, [])
+
+  // const handleLogout: Props = () => {
+  //   supabase.auth.signOut().then(() => {
+  //     setSession(null)
+  //     navigate('/')
+  //   })
+  // }
 
   useEffect(() => {
     if (session) {
--- swapp/src/components/NavBar/NavBar.tsx
+++ swapp/src/components/NavBar/NavBar.tsx
@@ -35,7 +35,6 @@ export default function NavBar() {
     setAccountMenu(null);
   };
 
-
   const renderAccountIcon = !['/', '/login'].includes(location.pathname);
 
   return (
