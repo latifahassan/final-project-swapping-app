e9d39924b00c51dd8b90bd433d6bf5d57f0e0259
alicefirminger <alicefirminger@gmail.com>
2023-06-21 11:28:46 +0100

    all tests now working

--- swapp/src/components/ListDisplay/ListDisplay.test.tsx
+++ swapp/src/components/ListDisplay/ListDisplay.test.tsx
@@ -15,11 +15,7 @@ beforeEach(() => {
 describe("ListDisplay", () => {
     test('renders correct number of DisplayCards', () => {
         const handleGetItNowClick = jest.fn();
-        render(<ListDisplay  numItems={3} handleGetItNowClick={handleGetItNowClick} searchResults={[
-          {title: 'test title 1', image: 'test1.jpg', username: 'test username 1', id: 'abc123'},
-          {title: 'test title 2', image: 'test2.jpg', username: 'test username 2', id: 'def456'},
-          {title: 'test title 3', image: 'test3.jpg', username: 'test username 3', id: 'ghi789'}
-         ]} spendATokenClicked={false} selectedItem={['abc123']} 
+        render(<ListDisplay  numItems={3} handleGetItNowClick={handleGetItNowClick} spendATokenClicked={false} selectedItem={['abc123']} 
          items={[
             {title: 'test title 1', image: 'test1.jpg', username: 'test username 1', item_id: 'abc123', user_id: 'abc123', created_at: '2023'},
             {title: 'test title 2', image: 'test2.jpg', username: 'test username 2', item_id: 'def456', user_id: 'abc123', created_at: '2023'},
