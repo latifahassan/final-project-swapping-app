113983cbfbe6f5bd14247f70acebe1e4dd0dc30c
NeilBrooks <neil.brooks87@gmail.com>
2023-06-21 12:24:45 +0100

    Commented out functions to avoid render failure

--- swapp/src/components/AuthPage/AuthUI.tsx
+++ swapp/src/components/AuthPage/AuthUI.tsx
@@ -7,7 +7,7 @@ type Props = {
     session: string;
   }
   
-export default function AuthUI(Props) {
+export default function AuthUI() {
 const navigate = useNavigate();
 const [activeTab, setActiveTab] = useState('login');
 const [loginEmail, setLoginEmail] = useState('');
@@ -15,22 +15,22 @@ const [loginPassword, setLoginPassword] = useState('');
 const [signupEmail, setSignupEmail] = useState('');
 const [signupPassword, setSignupPassword] = useState('');
 
-    const handleTabChange= (tab: string) => {
-        setActiveTab(tab);
-    }
+    // const handleTabChange= (tab: string) => {
+    //     setActiveTab(tab);
+    // }
 
-    const handleLogin = async () => {
-        const { user, session, error } = await supabase.auth.signInWithPassword({
-            email: loginEmail,
-            password: loginPassword
-        })
-        if (error) {
-            console.log(error)
-        } else {
-            console.log(user, session)
-            navigate('/home')
-        }   
-    }
+    // const handleLogin = async () => {
+    //     const { user, session, error } = await supabase.auth.signInWithPassword({
+    //         email: loginEmail,
+    //         password: loginPassword
+    //     })
+    //     if (error) {
+    //         console.log(error)
+    //     } else {
+    //         console.log(user, session)
+    //         navigate('/home')
+    //     }   
+    // }
 
     return (
         <div className="authUiContainer">
