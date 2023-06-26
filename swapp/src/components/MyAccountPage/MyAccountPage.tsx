99ab7cf65c0177e1c927ec5c5b1179e066410fa5
JownJawge <jgwoodard1718@gmail.com>
2023-06-26 10:17:34 +0100

    Added a filtering fuction to MyAccountPage to only show the current users items

--- swapp/src/components/MyAccountPage/MyAccountPage.tsx
+++ swapp/src/components/MyAccountPage/MyAccountPage.tsx
@@ -3,10 +3,11 @@
 // 2. A new list item area
 // 3. A list of my 'gets'/'claims'
 
-import React from 'react'
-import UploadItem from '../UploadItem/UploadItem'
-import ListDisplay from '../ListDisplay/ListDisplay'
-import { TableResults } from '../App/App'
+import React, { useEffect } from 'react';
+import UploadItem from '../UploadItem/UploadItem';
+import ListDisplay from '../ListDisplay/ListDisplay';
+import { TableResults } from '../App/App';
+import { useUser } from '@supabase/auth-helpers-react';
 
 type MyAccountPageProps = {
   items: TableResults[]
@@ -18,6 +19,16 @@ type MyAccountPageProps = {
 export default function MyAccountPage({ items, filteredItems, setFilteredItems }: MyAccountPageProps) {
 
   let numItems = 99;
+
+  const user = useUser();
+
+  useEffect( () => {
+    if(user) {
+      setFilteredItems(items.filter(x => x.user_id === user.id))
+    } else {
+      console.error("User does not have a value.")
+    };
+  }, [user, items, setFilteredItems] );
 
   return (
     <div className="accountContainer">
