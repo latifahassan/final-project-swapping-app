755afd3db3f2d2de42e880295d8679206351dd0c
gregor <gregor.aubrey.uk@gmail.com>
2023-06-26 14:03:42 +0100

    added console logs and comments to UploadItem.tsx, and edited the URL for the image that is inserted into the items table

--- swapp/src/components/UploadItem/UploadItem.tsx
+++ swapp/src/components/UploadItem/UploadItem.tsx
@@ -1,7 +1,6 @@
-import supabase from '../../supabaseClient';
-import React, { useState, useEffect } from 'react';
-import { v4 as uuid } from 'uuid';
-
+import supabase from "../../supabaseClient";
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function UploadItem() {
   const [user, setUser] = useState<any>(null);
@@ -11,75 +10,109 @@ export default function UploadItem() {
   const [uploadedFilePath, setUploadedFilePath] = useState("");
 
   useEffect(() => {
+    console.log("setUploadedFilePath:", uploadedFilePath);
+  }, [uploadedFilePath]);
+
+
+  useEffect(() => {
     async function fetchUser() {
-      const { data: { user } } = await supabase.auth.getUser();
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
       setUser(user);
       setIsUserLoaded(true);
     }
     fetchUser();
   }, []);
 
-console.log(images)
+  console.log(images);
 
   async function getImages() {
-    const { data, error } = await supabase
-      .storage
-      .from('images')
-      .list(user?.id + '/', { limit: 100, offset: 0, sortBy: { column: 'name', order: 'asc' } });
+    const { data, error } = await supabase.storage
+      .from("images")
+      .list(user?.id + "/", {
+        limit: 100,
+        offset: 0,
+        sortBy: { column: "name", order: "asc" },
+      });
     if (data) {
       console.log(data);
       setImages(data);
     } else {
       console.log(error);
-      alert('Error loading images.');
+      alert("Error loading images.");
     }
   }
 
   async function uploadImage(e: React.ChangeEvent<HTMLInputElement>) {
-
     e.preventDefault();
     let file = e.target.files?.[0];
     if (user && file) {
-        const filePath = user?.id + '/' + uuid();
-        const { data, error } = await supabase
-            .storage
-            .from('images')
-            .upload(filePath, file);
-        if (data) {
-            setUploadedFilePath(filePath);
-            getImages();
-        } else {
-            console.log(error);
-        }
+      // Create a unique filename for the uploaded file, and make the path the same as the current user's ID
+      const filePath = user?.id + "/" + uuid();
+      const { data, error } = await supabase.storage
+        .from("images")
+        .upload(filePath, file);
+      console.log("uploadImage function sent data to storage:", data);
+      if (data) {
+        setUploadedFilePath(filePath);
+        getImages();
+        console.log("getImages function got images:", images);
+      } else {
+        console.log(error);
+      }
     } else if (!user) {
-        alert('You must be logged in to upload an image.');
+      alert("You must be logged in to upload an image.");
     }
   }
-  
+
   async function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     if (user && title && uploadedFilePath) {
-        const {data: insertData, error: insertError} = await supabase
+      console.log(
+        "user:",
+        user?.id,
+        "title:",
+        title,
+        "uploadedFilePath:",
+        uploadedFilePath
+      );
+      const { data: insertData, error: insertError } = await supabase
         .from("items")
-        .insert([{title: title, user_id: user?.id, image: uploadedFilePath}]);
-        if(insertError) {
-            console.error(insertError)
-            alert("Error inserting new item.");
-        }
-        setTitle("");
-        getImages();
+        .insert([{ title: title, user_id: user?.id, image: `https://utocnplrsihspihnbpne.supabase.co/storage/v1/object/public/images/${uploadedFilePath}` }]);
+        console.log("insertData variable from handleFormSubmit:", insertData);
+      if (insertError) {
+        console.error(insertError);
+        alert("Error inserting new item.");
+      }
+      // Reset text input to empty string ready for next item that the user wants to list
+      setTitle("");
+      // Reset uploadedFilePath to empty string ready for next item that the user wants to list
+      setUploadedFilePath("");
+      getImages();
     } else if (!user) {
-        alert('You must be logged in to list an item.');
+      alert("You must be logged in to list an item.");
     }
-}
+  }
 
-
-return (
-  <form onSubmit={handleFormSubmit}>
-    <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-    <input type="file" name="file" accept="image/png,image/jpeg,image/jpg,image/webp" onChange={uploadImage} />
-    <button type="submit" disabled={!isUserLoaded || !uploadedFilePath}>List it!</button>
-    {!isUserLoaded && <p>Loading user session...</p>}
-  </form>
-);
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="file"
+        name="file"
+        accept="image/png,image/jpeg,image/jpg,image/webp"
+        onChange={uploadImage}
+      />
+      <button type="submit" disabled={!isUserLoaded || !uploadedFilePath}>
+        List it!
+      </button>
+      {!isUserLoaded && <p>Loading user session...</p>}
+    </form>
+  );
 }
